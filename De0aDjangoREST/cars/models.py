b716687529344db89287aea1ea6f94531c4b2c6f
Miguel Jiménez <miguel1315@gmail.com>
2020-01-08 16:56:48 +0100

    Init Cars app models

--- De0aDjangoREST/cars/models.py
+++ De0aDjangoREST/cars/models.py
@@ -1,3 +1,30 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+
+class Brand(models.Model):
+    """
+    Brand definition of cars
+    """
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=15)
+
+
+class Car(models.Model):
+    """
+    Car definition
+    """
+    uuid = models.UUIDField(
+        default=uuid.uuid4,
+        editable=False
+    )
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    model = models.CharField(max_length=25)
+    brand = models.ForeignKey(
+        'Brand',
+        related_name='cars',
+        on_delete=models.CASCADE
+    )
