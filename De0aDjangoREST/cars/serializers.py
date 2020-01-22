00f76ab3ff0c867b0918ca8675309fb68d5160bf
Miguel Jiménez <miguel1315@gmail.com>
2020-01-22 13:13:45 +0100

    Improve serializers

--- De0aDjangoREST/cars/serializers.py
+++ De0aDjangoREST/cars/serializers.py
@@ -1,10 +1,26 @@
 from rest_framework import serializers
 
-from .models import Brand
+from .models import Brand, Car
 
 
 class BrandSerializer(serializers.ModelSerializer):
+    total_cars = serializers.SerializerMethodField()
 
     class Meta:
         model = Brand
-        fields = ('id', 'name', )
+        fields = ('id', 'name', 'total_cars', )
+        read_only_fields = ('id', )
+
+    def get_total_cars(self, obj):
+        return obj.cars.count()
+
+
+class CarSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Car
+        fields = ('id', 'model', 'brand', 'manufacturing_date', )
+        read_only_fields = ('id', )
+        extra_kwargs = {
+            'manufacturing_date': {'write_only': True},
+        }
