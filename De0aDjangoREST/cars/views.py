28da5e42cffea06465a119610fe1fabd86dea8a8
Miguel Jiménez <miguel1315@gmail.com>
2020-09-06 11:03:13 +0200

    Add filters

--- De0aDjangoREST/cars/views.py
+++ De0aDjangoREST/cars/views.py
@@ -1,37 +1,41 @@
-from rest_framework.generics import (CreateAPIView, DestroyAPIView, ListAPIView,
-                                     RetrieveAPIView, UpdateAPIView)
+from rest_framework import generics
+from rest_framework import filters as df
 
 from .models import Brand
 from .paginations import SmallResultsSetPagination
 from .serializers import BrandSerializer
 
 
-class BrandListView(ListAPIView):
+class BrandListView(generics.ListAPIView):
     serializer_class = BrandSerializer
     permission_classes = ()
     queryset = Brand.objects.all()
     pagination_class = SmallResultsSetPagination
+    filter_backends = (df.OrderingFilter, df.SearchFilter, )
+    search_fields = ('name', )
+    ordering_fields = ('name', )
 
-class BrandCreateView(CreateAPIView):
+
+class BrandCreateView(generics.CreateAPIView):
     serializer_class = BrandSerializer
     permission_classes = ()
 
 
-class BrandRetrieveView(RetrieveAPIView):
-    serializer_class = BrandSerializer
-    permission_classes = ()
-    queryset = Brand.objects.all()
-    lookup_field = 'id'
-
-
-class BrandUpdateView(UpdateAPIView):
+class BrandRetrieveView(generics.RetrieveAPIView):
     serializer_class = BrandSerializer
     permission_classes = ()
     queryset = Brand.objects.all()
     lookup_field = 'id'
 
 
-class BrandDestroyView(DestroyAPIView):
+class BrandUpdateView(generics.UpdateAPIView):
+    serializer_class = BrandSerializer
+    permission_classes = ()
+    queryset = Brand.objects.all()
+    lookup_field = 'id'
+
+
+class BrandDestroyView(generics.DestroyAPIView):
     serializer_class = BrandSerializer
     permission_classes = ()
     queryset = Brand.objects.all()
