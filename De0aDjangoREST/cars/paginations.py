bc8ebe5203478b2038ccb9dc87199ab88d511ae8
Miguel Jiménez <miguel1315@gmail.com>
2020-09-06 10:24:32 +0200

    Little improvement in paginations

--- De0aDjangoREST/cars/paginations.py
+++ De0aDjangoREST/cars/paginations.py
@@ -4,5 +4,3 @@ from rest_framework.pagination import PageNumberPagination
 class SmallResultsSetPagination(PageNumberPagination):
     page_size = 10
     page_query_param = 'page'
-    page_size_query_param = 'page_size'
-    max_page_size = 10
