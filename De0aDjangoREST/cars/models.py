b83b1797e25e52af194e979b7ff549f91f72c063
Miguel Jiménez <miguel1315@gmail.com>
2020-09-15 19:55:16 +0200

    Fix model Car and migrations

--- De0aDjangoREST/cars/models.py
+++ De0aDjangoREST/cars/models.py
@@ -11,20 +11,18 @@ class Brand(models.Model):
     updated = models.DateTimeField(auto_now=True)
     name = models.CharField(max_length=15)
 
-
 class Car(models.Model):
-    """
-    Car definition
-    """
-    uuid = models.UUIDField(
-        default=uuid.uuid4,
-        editable=False
-    )
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     model = models.CharField(max_length=25)
+    manufacturing_date = models.DateField()
     brand = models.ForeignKey(
         'Brand',
         related_name='cars',
         on_delete=models.CASCADE
     )
+
+    class Meta:
+        ordering = ['updated']
+        indexes = [models.Index(fields=['brand'])]
+
